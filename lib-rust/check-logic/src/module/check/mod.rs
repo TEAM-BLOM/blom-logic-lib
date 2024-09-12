4bb944b712d0720fd1bed103816d88a520ee4eef
Seung <gccs457@naver.com>
2024-09-12 14:50:08 +0900

    temp: dev

--- lib-rust/check-logic/src/module/check/mod.rs
+++ lib-rust/check-logic/src/module/check/mod.rs
@@ -40,6 +40,11 @@ impl Check {
         self.current = (x, y);
     }
 
+    pub fn set_new_board(&mut self, _board: Board, x: usize, y: usize) {
+        self.board = _board;
+        self.current = (x, y);
+    }
+
     pub fn check_win(&self) -> Result<bool, JsError> {
         let (_x, _y) = self.current;
         let mut win: bool = false;
@@ -105,5 +110,40 @@ impl Check {
         }
 
         return Ok(win);
+    }
+
+    pub fn check_33(&self) -> Result<bool, JsError> {
+        let (_x, _y) = self.current;
+        let dir: [(i32, i32); 8] = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, -1), (-1, 1), (1, -1)];
+
+        let tmp = self.board.convert_to_index(_x, _y);
+        if self.board.get_board()[tmp] == -1 {
+            return Ok(false);
+        } else if self.board.get_board()[tmp] == 0 {
+            return Err(JsError::new("this position looks Invalid"));
+        }
+
+        for i in (0..8).step_by(2) {
+            let dx_1 = dir[i].0;
+            let dy_1 = dir[i].1;
+            let mut cnt_1: i32 = 0;
+            
+            let mut x = _x;
+            let mut y = _y;
+            // direction 1
+            loop {
+                x = add_usize_int32(x, dx_1).unwrap();
+                y = add_usize_int32(y, dy_1).unwrap();
+                let idx = self.board.convert_to_index(x, y);
+
+                if self.board.is_valid(x, y) && self.board.get_board()[idx] == 1 {
+                    cnt_1 += 1;
+                } else {
+                    break;
+                }
+            }
+        }
+
+
     }
 }
